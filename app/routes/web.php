fdf5e02a203e6ccb3849885995aba401b4d799c7
Hamzah SM <hamzahsaiful782@gmail.com>
2022-11-15 16:07:20 +0700

    modified route

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 
+Route::get('/form', function(){
+    return view('form');
+});
+
+// for upload image
+Route::post('ckeditor/upload', 'CKEditorController@upload')->name('ckeditor.image-upload');
